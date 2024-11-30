15e8da2be2b6760a370b48f37cbc095c0ba22a14
DevJose <devjose@email.com>
2024-11-29 21:48:15 -0600

    Configurar pantalla de elimnar forzado

---
+++ lib/pages/remove_forzado/screen/home_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class HomePageRemove extends StatelessWidget {
+  const HomePageRemove({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(items: []),
+    );
+  }
+}
