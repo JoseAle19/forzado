ca22224977cbb437d6d2d7e76ee2ac5097b5a9ca
DevJose <devjose@email.com>
2024-12-01 01:37:10 -0600

    dev

--- lib/core/urls.dart
+++ lib/core/urls.dart
@@ -1,7 +1,7 @@
 class AppUrl {
   // Base URL del servidor
   static const String url =
-      "https://sntps2jn-3000.brs.devtunnels.ms"; // Cambia según tu configuración
+      "https://sntps2jn-3001.brs.devtunnels.ms"; // Cambia según tu configuración
 
   // Endpoints  para vitarme equivocacion de rutas
   static const String login = "api/mobile/auth/";
